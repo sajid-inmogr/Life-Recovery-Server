fc5a036f4cc2eec1bb96044014aa1f31b4e8a82e
shahriyarhosen <me.shahriyarhosen@gmail.com>
2022-09-26 19:57:25 +0600

    models done

---
+++ models/Isha.js
@@ -0,0 +1,25 @@
+import mongoose from "mongoose";
+const IshaSchema = new mongoose.Schema(
+  {
+    email: {
+      type: String,
+      required: true,
+      unique: true,
+    },
+    complete: {
+      type: Boolean,
+      default: false,
+    },
+    Jamaat: {
+      type: Boolean,
+      default: true,
+    },
+    takbir_e_ula: {
+      type: Boolean,
+      default: true,
+    },
+  },
+  { timestamps: true }
+);
+
+export default mongoose.model("Isha", IshaSchema);
--- models/User.js
+++ models/User.js
@@ -3,7 +3,6 @@ const UserSchema = new mongoose.Schema(
   {
     username: {
       type: String,
-      required: true,
       unique: true,
     },
     email: {
@@ -18,10 +17,6 @@ const UserSchema = new mongoose.Schema(
     img: {
       type: String,
     },
-    city: {
-      type: String,
-      // required: true,
-    },
     phone: {
       type: String,
       // required: true,
@@ -29,10 +24,6 @@ const UserSchema = new mongoose.Schema(
     password: {
       type: String,
       required: true,
-    },
-    isAdmin: {
-      type: Boolean,
-      default: false,
     },
   },
   { timestamps: true }
